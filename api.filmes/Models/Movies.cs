9bf2fc9b203183715ce39c7e787ad5bf8d57a05b
marcinho82 <marcioandredasilva@bol.com.br>
2020-07-10 01:52:50 -0300

    commit

---
+++ api.filmes/Models/Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.filmes.Models
+{
+    public class Movies
+    {
+        public int movieId { get; set; }
+        public string nome { get; set; }
+        public int ano_lancamento { get; set; }
+        public string imagem { get; set; }
+        public string slogan { get; set; }
+        public string visaoGeral { get; set; }
+        public string classificacao { get; set; }
+    }
+}
---
+++ api.filmes/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.filmes.Models
+{
+    public class Users
+    {
+        public int userId { get; set; }
+        public string email { get; set; }
+    }
+}
